92a2550621c2376572f528660299244cb0a02ede
sekuritaskan.hanif <sekuritaskan.hanif@gmail.com>
2025-02-08 08:18:33 +0000

    feat(*): added .idx
    Signed-off-by: sekuritaskan.hanif <sekuritaskan.hanif@gmail.com>

---
+++ .idx/dev.nix
@@ -0,0 +1,15 @@
+{pkgs, ...}: {
+    packages = [
+        pkgs.go_1_22
+        pkgs.nodejs-slim_21
+        pkgs.github-cli
+    ];
+
+    # Enabling MySQL
+    services.mysql.enable = true;
+    services.mysql.package = pkgs.mysql80;
+
+    # Enabling Redis
+    services.redis.enable = true;
+    services.redis.port = 6335;
+}
